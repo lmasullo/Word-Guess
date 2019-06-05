ac758563c1737cc171a1cb42868283a5e979db1c
Lawrence N. Masullo <lmasullo@gmail.com>
2019-06-05 11:02:09 -0500

    added multiple indices

--- assets/js/game.js
+++ assets/js/game.js
@@ -39,28 +39,45 @@ function randSong() {
 // Call the random word generator and set the word to use
 const newSong = randSong();
 
-// Loop over the word and get each character and add to an array
+// Loop over the random word and get each character and add to an array
 const arrWord = [];
 for (let i = 0; i < newSong.length; i++) {
   arrWord.push(newSong.charAt(i));
-  // Display the underlines for the word picked
+  // Display the underlines for the word picked, based on array lenght
   document.getElementById('blankWord').innerHTML += ' _ ';
 }
 
+// Display the array
 console.log(arrWord);
 
 // Create an array to hold all the correctly guessed letters
 // Make it the same length as the current word
 const arrCorrect = new Array(arrWord.length);
 
-// Function that gets the pressed key,
-// function keyPressed(event) {
+// Function that gets the pressed key
+// Use keyup so it is a complete key press
 document.onkeyup = function keyPressed(event) {
+  // event.key captures the pressed key
   const x = event.key;
   console.log(x);
 
   // Check if letter is in current word
   const letterCheck = arrWord.indexOf(x);
+
+  // Extend the prototype Array
+  Array.prototype.multiIndexOf = function (el) { 
+    var idxs = [];
+    for (var i = this.length - 1; i >= 0; i--) {
+        if (this[i] === el) {
+            idxs.unshift(i);
+        }
+    }
+    return idxs;
+};
+
+let justIdx = arrWord.multiIndexOf(x);
+ 
+console.log(justIdx);
 
   // If not in the word decrement the guesses left
   if (letterCheck === -1) {
