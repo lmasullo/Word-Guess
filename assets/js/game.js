1f19bf6b4da770c12959ff7bbda8cc7153abdeaa
Larry Masullo <lmasullo@gmail.com>
2019-06-06 18:05:55 -0500

    Fixed looking for multiple letters

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,19 @@
 console.log('Connected!');
 
 // Set the wins variable
-const intWins = 0;
+let intWins = 0;
 document.getElementById('wins').innerHTML = intWins;
 
 // Set the losses variable
-const intLosses = 0;
+let intLosses = 0;
 document.getElementById('losses').innerHTML = intLosses;
 
 // Set the guesses remaining variable
 let intGuesses = 12;
 document.getElementById('guessesLeft').innerHTML = intGuesses;
+
+// Create array to hold the guesses
+let arrGuesses = [];
 
 // Random word chooser
 function randSong() {
@@ -33,22 +36,39 @@ function randSong() {
   ];
 
   const song = songs[Math.floor(Math.random() * songs.length)];
-  return song;
+
+  // Loop over the random word and get each character and add to an array
+  // Create the array to hold the current word
+  const arrWord = [];
+  for (let i = 0; i < song.length; i += 1) {
+    arrWord.push(song.charAt(i));
+    // Display the underlines for the word picked, based on array lenght
+    document.getElementById('blankWord').innerHTML += ' _ ';
+  }
+  // Display the array
+  console.log(arrWord);
+
+  return arrWord;
 }
 
-// Call the random word generator and set the word to use
-const newSong = randSong();
+// Function that resets all
+function reset() {
+  // Set the guesses remaining variable
+  intGuesses = 12;
+  document.getElementById('guessesLeft').innerHTML = intGuesses;
 
-// Loop over the random word and get each character and add to an array
-const arrWord = [];
-for (let i = 0; i < newSong.length; i++) {
-  arrWord.push(newSong.charAt(i));
-  // Display the underlines for the word picked, based on array lenght
-  document.getElementById('blankWord').innerHTML += ' _ ';
+  // Clear the array
+  arrGuesses = [];
+  document.getElementById('dispGuesses').innerHTML = '';
+
+  // Clear the span of the underlines
+  document.getElementById('blankWord').innerHTML = '';
+
+  // Call the random letter generator
+  randSong();
 }
-
-// Display the array
-console.log(arrWord);
+// Call the random word generator
+const arrWord = randSong();
 
 // Create an array to hold all the correctly guessed letters
 // Make it the same length as the current word
@@ -59,13 +79,38 @@ const arrCorrect = new Array(arrWord.length);
 document.onkeyup = function keyPressed(event) {
   // event.key captures the pressed key
   const x = event.key;
-  console.log(x);
+  // console.log(x);
+
+  // Add to array of guesses if not already in the array
+  // const arrCheck = arrGuesses.indexOf(x);
+  // console.log(arrCheck);
+  // if (arrCheck === -1) {
+  //   arrGuesses.push(x);
+  //   console.log(arrGuesses);
+
+  //   // Display the remaining guesses
+  //   // Decrement by one
+  //   intGuesses -= 1;
+  //   document.getElementById('guessesLeft').innerHTML = intGuesses;
+
+  //   // Display the guesses so far
+  //   document.getElementById('dispGuesses').innerHTML = arrGuesses;
+
+  //   // If remaining guesses gets to 0, increment losses and reset
+  //   if (intGuesses === 0) {
+  //     intLosses += 1;
+  //     document.getElementById('losses').innerHTML = intLosses;
+
+  //     // Call Reset function
+  //     reset();
+  //   }
+  // }
 
   // Extend the prototype Array
   // to get all the indexes if multiple instances of the letter chosen
   Array.prototype.multiIndexOf = function (el) {
     const idxs = [];
-    for (let i = this.length - 1; i >= 0; i--) {
+    for (let i = this.length - 1; i >= 0; i -= 1) {
       if (this[i] === el) {
         idxs.unshift(i);
       }
@@ -73,16 +118,34 @@ document.onkeyup = function keyPressed(event) {
     return idxs;
   };
 
-  // Check if letter is in current word
+  // Check if letter is in current word (checkes for multiples)
   const letterCheck = arrWord.multiIndexOf(x);
-  console.log(`Idx: ${letterCheck}`);
+  // console.log(letterCheck.length);
 
   // If not in the word decrement the guesses left
   if (letterCheck.length === 0) {
+    // Check if the letter is already in guesses array
+    const arrCheck = arrGuesses.indexOf(x);
+    if (arrCheck === -1) {
+      arrGuesses.push(x);
+    }
+    // console.log(arrGuesses);
+
     // Display the remaining guesses
     // Decrement by one
     intGuesses -= 1;
     document.getElementById('guessesLeft').innerHTML = intGuesses;
+
+    // Display the guesses so far
+    document.getElementById('dispGuesses').innerHTML = arrGuesses;
+
+    if (intGuesses === 0) {
+      intLosses += 1;
+      document.getElementById('losses').innerHTML = intLosses;
+
+      // Call Reset function
+      reset();
+    }
 
     // Chosen letter is in the current word
   } else {
@@ -104,5 +167,18 @@ document.onkeyup = function keyPressed(event) {
         document.getElementById('blankWord').innerHTML += arrCorrect[i];
       }
     }
-  }
-};
+
+    // Check if the correct choices array as any empty indicies
+    // If all full, player has guessed all the correct letters and wins!
+    const arrCheckWin = arrCorrect.includes(undefined);
+    // console.log(arrCheckWin);
+    if (arrCheckWin === false) {
+      // Increment the wins
+      intWins += 1;
+      document.getElementById('wins').innerHTML = intWins;
+
+      // Call Reset function
+      reset();
+    }
+  } // End check if letter is in the word
+}; // End Key Pressed
